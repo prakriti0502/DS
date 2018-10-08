4500f7254d9ca3d47cb1fa0f2a3cefa0790cb7e3
prakriti0502 <Prakriti.sagar@icloud.com>
2018-10-08 23:13:29 +0530

    Heap Implementation done

--- HeapImplementation.cpp
+++ HeapImplementation.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stack>
 using namespace std;
 class heap
 {
@@ -14,7 +15,7 @@ class heap
 			F=0;
 			l=2*i;
 			r=(2*i)+1;
-			if(l<=n && arr[l]>arr[r])
+			if(l<=n && arr[l]>arr[i])
 			{
 				F=1;
 				largest = l;
@@ -35,6 +36,72 @@ class heap
 				max_heapify(arr,largest,n);
 			}
 		}
+		int delete_(int arr[],int &n)
+		{
+			int save;
+			save = arr[1];
+			arr[1] = arr[n];
+			n--;
+			max_heapify(arr,1,n);
+			return save;
+		}
+		void insert(int arr[],int &n)
+		{
+			int ele,temp;
+			cout<<"Enter what element you want to insert\n";
+			cin>>ele;
+			n++;
+			arr[n] = ele;
+			int i = n;
+			while(arr[i/2] < arr[i] && i > 1)
+			{
+				temp = arr[i / 2];
+				arr[i/2] = arr[i];
+				arr[i] = temp;
+				i = i / 2;
+			}
+		}
+		void update(int arr[],int n)
+		{
+			int index,new_,temp;
+			cout<<"Enter on what index you want the new value to be inserted and the value of new element\n";
+			cin>>index>>new_;
+			if(new_>arr[index])
+			{
+				arr[index] = new_;
+				int i = index;
+				while(arr[i/2] < arr[i] && i > 1)
+				{
+					temp = arr[i / 2];
+					arr[i/2] = arr[i];
+					arr[i] = temp;
+					i = i / 2;
+				}
+			}
+			else
+			{
+				arr[index] = new_;
+				max_heapify(arr,index,n);
+			}
+		}
+		void heap_sort(int arr[],int n)
+		{
+			stack <int> ms;
+			while(n>0)
+			{
+				int save = arr[1];
+				ms.push(save);
+				arr[1] = arr[n];
+				n--;
+				max_heapify(arr,1,n);
+			}
+			while(!ms.empty())
+			{
+				cout<<ms.top()<<"\t";
+				ms.pop();
+			}
+			cout<<endl;
+		}
 };
 int main()
 {
@@ -48,17 +115,55 @@ int main()
 	{
 		cin>>arr[i];
 	}
-	int choice;
-	cout<<"Enter 1 to create max heap\n";
-	cin>>choice;
-	switch(choice)
+	int choice,c=0;
+	char cont;
+	cout<<"Enter y to continue and n to exit\n";
+	cin>>cont;
+	while(cont!='n')
 	{
-		case 1:
-			{
-				obj.build_max_heap(arr,n);
-			}
+		cout<<"Enter 1 to create max heap\n";
+		cout<<"Enter 2 for deletion\n";
+		cout<<"Enter 3 for insertion of new element\n";
+		cout<<"Enter 4 for updation of any element\n";
+		cout<<"Enter 5 for sorting in ascending order\n";
+		cin>>choice;
+		switch(choice)
+		{
+			case 1:
+				{
+					obj.build_max_heap(arr,n);
+					break;
+				}
+			case 2:
+				{
+					obj.delete_(arr,n);
+					break;
+				}
+			case 3:
+				{
+					obj.insert(arr,n);
+					break;
+				}
+			case 4:
+				{
+					obj.update(arr,n);
+					break;
+				}
+			case 5:
+				{
+					obj.heap_sort(arr,n);
+					c = 1;
+					break;
+				}
+		}
+		if(c==0)
+		{
+			for(int i=1;i<=n;i++)
+				cout<<arr[i]<<"\t";
+		}
+		cout<<endl;
+		cout<<"Enter y to continue and n to exit\n";
+		cin>>cont;
 	}
-	for(int i=1;i<=n;i++)
-		cout<<arr[i]<<"\t";
 	return 0;
 }
