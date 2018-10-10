39e351a6d3186b9f900a354486f50e17b64c62c2
prakriti0502 <Prakriti.sagar@icloud.com>
2018-10-10 23:46:36 +0530

    Graoh

--- GraphsImplementationUsingAdjencyList.cpp
+++ GraphsImplementationUsingAdjencyList.cpp
@@ -3,22 +3,77 @@ using namespace std;
 struct node
 {
 	int data;
-	int *node;
+	node *next;
 };
 class GRAPH
 {
 	int v,e;
-	node *start;
+	node **start;
 	public:
-		GRAPH(int ver)
+		GRAPH(int ver,int edg)
 		{
-			node *start = new int[ver];
+			start = new node*[ver];
+			v = ver;
+			e = edg;
+		}
+		void create_graph()
+		{
+			int val;
+			for(int i=0;i<v;i++)
+			{
+				start[i] = NULL;
+			}
+			for(int i=0;i<v;i++)
+			{
+				cout<<"Enter the nodes to be connected for "<<i<<" node and press -1 to stop\n";
+				cin>>val;
+				node *temp = new node;
+				while(val != -1)
+				{
+					temp->data = val;
+					temp->next = NULL;
+					if(start[i] == NULL)
+					{
+						start[i] = temp;
+						//cout<<"chaling\n";
+					}
+					else
+					{
+						node *ptr;
+						ptr=start[i];
+						while(ptr!= NULL)
+							ptr = ptr->next;
+						ptr= temp;
+					//	ptr = temp;
+					}
+					cin>>val;
+				}
+			}
+		}
+		void display()
+		{
+			node *ptr;
+			cout<<"The adjancy list is\n";
+			for(int i=0;i<v;i++)
+			{
+				ptr = start[i];
+				while(ptr!=NULL)
+				{
+					cout<<ptr->data<<"\t";
+					ptr = ptr->next;
+					//cout<<"kyuji";
+				}
+				//cout<<"\nHaw";
+				cout<<endl;
+			}
 		}
 };
 int main()
 {
-	int ver;
-	cout<<"Enter number of vertices\n";
-	cin>>ver;
-	GRAPH g(ver);
+	int ver,edg;
+	cout<<"Enter number of vertices and edges\n";
+	cin>>ver>>edg;
+	GRAPH g(ver,edg);
+	g.create_graph();
+	g.display();
 }
