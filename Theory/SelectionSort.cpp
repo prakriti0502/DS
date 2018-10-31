d223f5f42d09233cd2a83fb6f040a500e50ca58e
prakriti0502 <Prakriti.sagar@icloud.com>
2018-10-31 15:28:10 +0530

    Selection Sort done

---
+++ Theory/SelectionSort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter size\n";
+    cin>>n;
+    int j;
+    int arr[100];
+    int index;
+    cout<<"Enter array elements\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+ for(int i=0;i<n;i++)
+ {
+   index=i;
+   for(int j=i+1;j<n;j++)
+   {
+    if(arr[j]<arr[index])
+    {
+      index=j;
+    }
+   }
+   if(index!=i)
+   {
+    int t=arr[i];
+    arr[i]=arr[index];
+    arr[index]=t;
+   }
+    cout<<arr[i];
+ }
+
+ return 0;
+}
